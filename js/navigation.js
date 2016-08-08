5667eb5dcd437eabc0235ccf76ec8e8a0f00334b
Wonjun Park <pwjun23@gmail.com>
2016-08-08 18:38:44 +0900

    160808_박원준 진행
    전체 메뉴 추가 및 서브메뉴 개선

--- js/navigation.js
+++ js/navigation.js
@@ -16,37 +16,42 @@
 
   var $depth2Title = $('.depth2Bg h2');
 
+  var $allMenu =$('.allMenu');
+
+  var $allMenuDepth1 = $allMenu.children('.container').children('ul').addClass('depth1');
+
+  var $allMenuDepth2 = $allMenuDepth1.children('li').children('ul').addClass('depth2');
+
+  var $btnAllMenu = $('.btn-allMenu');
+
   settingRoleProperties();
+  settingAllMenuRoleProperties();
 
   // ------------------------------------------------------------------
   // Handler
   function navigationActive(e) {
-    console.log('navigationActive');
+    //console.log('navigationActive');
     allDeactive();
     $(this).parent().addClass('active');
     var $color = $(this).css('color');
     var $depth2TitleText = $(this).html();
-
     $depth2Title.html($depth2TitleText);////수원U-city디자인에 따른 추가 코드 - 뎁스2 하단 서브메뉴 타이틀
     $depth2Bg.addClass('active');//수원U-city디자인에 따른 추가 코드 - 서브메뉴 크기    
     $depth2Bg.css('border-color',$color);//수원U-city디자인에 따른 추가 코드 - 서브메뉴 선 색상
-
   }
 
   function navigationDeactive(e) {
-    console.log('navigationDeactive');
+    //console.log('navigationDeactive');
     allDeactive();
-
   }
 
   function allDeactive() {
-    console.log('allDeactive');
+    //console.log('allDeactive');
     $nav.find('.depth1 > li').removeClass('active');
     $depth2Bg.removeClass('active');//수원U-city디자인에 따른 추가 코드 
   }
 
   function settingRoleProperties() {
-
     // <div role="navigation">
     if(!$nav.is('nav')) {
       $nav.attr('role', 'navigation');
@@ -56,16 +61,45 @@
     $allLinks.attr('role', 'menu-item');
   }
 
-  function depth2BgDeactive(evt){
-    evt.stopPropagation();
-    console.log('depth2BgDeactive');
-    return false;
-    allDeactive();    
+  function settingAllMenuRoleProperties(){
+
+    $allMenuDepth2.children('li').each(function(idx){
+    //  console.log($(this).find('ul').is('ul'));
+      if($(this).find('ul').is('ul')){
+        //console.log('has ul!!');
+        $(this).children('a').css('pointer-events','none');  
+      }
+    });
+    /*if($allMenuDepth2.has('ul')){
+      console.log('has ul!!');
+      $allMenuDepth2.children('li').children('a').css('pointer-events','none');  
+    }*/
+  }
+
+  function depth2Bgleave(evt){
+    console.log(evt.target,evt.currentTarget);
+    if(evt.target == evt.currentTarget){
+      console.log('depth2Bgleave');
+      allDeactive();  
+    }
   }
 
   // ------------------------------------------------------------------
   // Event Binding (Event + Handler)
-  $nav.on('mouseenter focus', '.depth1 > li > a', navigationActive);
+  $nav.on('mouseenter focus', '.depth1 > li > a', navigationActive);  
   $nav.on('blur', 'a:last', navigationDeactive);
-  //$depth2Bg.on('mouseenter mouseleave', depth2BgDeactive);
+  $depth2Bg.on('mouseleave', function(evt) {//서브 메뉴 이벤트 붙이기
+    if(evt.target == evt.currentTarget){
+      //console.log('depth2Bgleave');
+      allDeactive();  
+    }  
+  });
+  $btnAllMenu.on('mousedown', function(){
+    allDeactive();
+    if(!$allMenu.hasClass('active')){
+      $allMenu.addClass('active');
+    }else{
+      $allMenu.removeClass('active');
+    }
+  })
 })(this);
